269f0521dfd1df34a7f031898887f3952c8372cb
dongwonyang <121853823+dongwonyang@users.noreply.github.com>
2025-12-18 17:29:11 +0900

    build: naver map 의존성 추가, internet 허용

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,12 +1,20 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.example.android.application)
 }
 
+val localProperties = Properties()
+localProperties.load(project.rootProject.file("local.properties").inputStream())
+
+
 android {
     defaultConfig {
         applicationId = "project.priceit"
+        resValue("string", "naver_client_id", localProperties.getProperty("naver_client_id"))
     }
 }
+
 
 dependencies {
     implementation(projects.core.designsystem)
--- feature/home/build.gradle.kts
+++ feature/home/build.gradle.kts
@@ -1,6 +1,14 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.example.feature)
 }
 
 dependencies {
+    // Naver Maps
+    implementation(libs.naver.map.compose)
+    implementation(libs.naver.map.location)
+
+    // Permissions
+    implementation(libs.accompanist.permissions)
 }
