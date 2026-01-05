97b91ee9867b138fe9fe93a666b37da487b55ebb
dongwonyang <121853823+dongwonyang@users.noreply.github.com>
2026-01-05 19:09:35 +0900

    refac/#2: HomeScreen coponenet로 정리
    listSection, searchSection 추가, MapSection dimens.RoundCommon 사용 적용,

--- feature/home/src/main/kotlin/project/priceit/feature/home/HomeScreen.kt
+++ feature/home/src/main/kotlin/project/priceit/feature/home/HomeScreen.kt
@@ -1,27 +1,15 @@
 package project.priceit.feature.home
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Place
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -29,23 +17,18 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import project.priceit.core.designsystem.component.CommonButton
-import project.priceit.core.designsystem.component.CommonEditTextBox
 import project.priceit.core.designsystem.component.CommonListItem
 import project.priceit.core.designsystem.theme.Dimens
+import project.priceit.feature.home.component.ListSection
 import project.priceit.feature.home.component.MapSection
 import project.priceit.feature.home.component.RadiusSettingDialog
+import project.priceit.feature.home.component.SearchSection
 
 @Composable
 internal fun HomeRoute(
@@ -108,43 +91,21 @@ private fun HomeScreen(
                     onMartClick = {},
                     onShowRadiusDialog = viewModel::showRadiusDialog,
                 )
+                Spacer(modifier = Modifier.height(Dimens.DpMedium))
 
-                Spacer(modifier = Modifier.height(12.dp))
+                SearchSection {  }
+                Spacer(modifier = Modifier.height(Dimens.DpMedium))
 
-                // Search section
-                var query by remember { mutableStateOf("") }
-                Text(text = "장소 검색", fontWeight = FontWeight.Bold)
-                Spacer(modifier = Modifier.height(6.dp))
-
-                CommonEditTextBox(
-                    value = query,
-                    onValueChange = { query = it },
-                    placeHolder = "장소를 입력하세요.",
-                    modifier = Modifier.fillMaxWidth(),
+                ListSection(
+                    title = "현재 진행 중인 의뢰",
+                    items = sampleRequests,
                 )
+                Spacer(modifier = Modifier.height(Dimens.DpMedium))
 
-                Spacer(modifier = Modifier.height(8.dp))
-
-
-                CommonButton(
-                    text = "검색",
-                    onClick = { /* 검색 동작 */ },
-                    modifier = Modifier
-                        .fillMaxWidth()
+                ListSection(
+                    title = "추천의뢰",
+                    items = sampleRequests,
                 )
-
-                Spacer(modifier = Modifier.height(Dimens.DpSmall))
-
-                // Current requests
-                Text("현재 진행 중인 의뢰", fontWeight = FontWeight.Bold)
-                Spacer(modifier = Modifier.height(Dimens.DpSmall))
-                RequestList(items = sampleRequests)
-
-                Spacer(modifier = Modifier.height(12.dp))
-
-                Text("추천 의뢰", fontWeight = FontWeight.Bold)
-                Spacer(modifier = Modifier.height(Dimens.DpSmall))
-                RequestList(items = sampleRequests)
             }
         }
     }
