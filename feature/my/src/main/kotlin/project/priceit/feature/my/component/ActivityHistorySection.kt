22d60dc5c6edae20b80e560ec9922313994b5e98
dongwonyang <121853823+dongwonyang@users.noreply.github.com>
2026-01-22 16:16:45 +0900

    feat/#14: section 뼈대 구현

---
+++ feature/my/src/main/kotlin/project/priceit/feature/my/component/ActivityHistorySection.kt
@@ -0,0 +1,24 @@
+package project.priceit.feature.my.component
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ActivityHistorySection() {
+}
+
+@Composable
+fun ActivityBox() {
+}
+
+@Composable
+@Preview
+fun ActivityHistorySectionPrev() {
+    ActivityHistorySection()
+}
+
+@Composable
+@Preview
+fun ActivityBoxPrev() {
+    ActivityBox()
+}
---
+++ feature/my/src/main/kotlin/project/priceit/feature/my/component/ProfileDialog.kt
@@ -0,0 +1,14 @@
+package project.priceit.feature.my.component
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ProfileDialog() {
+}
+
+@Composable
+@Preview
+fun ProfileDialogPreview() {
+    ProfileDialog()
+}
