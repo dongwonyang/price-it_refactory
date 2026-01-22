9d6eb2ec1dcabc8da15097f2b1920414e2356bca
dongwonyang <121853823+dongwonyang@users.noreply.github.com>
2026-01-22 16:17:53 +0900

    feat/#14: profileSection 구현, uiState에 해당 section UIState 추가

--- feature/my/src/main/kotlin/project/priceit/feature/my/MyScreen.kt
+++ feature/my/src/main/kotlin/project/priceit/feature/my/MyScreen.kt
@@ -1,10 +1,21 @@
 package project.priceit.feature.my
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import project.priceit.core.designsystem.component.GrayDivider
+import project.priceit.core.designsystem.theme.Dimens
+import project.priceit.feature.my.component.ProfileSection
 import project.priceit.feature.my.model.MyEvent
 import project.priceit.feature.my.model.MyUiState
 
@@ -36,5 +47,19 @@ fun MyScreen(
     uiState: MyUiState,
     onEvent: (MyEvent) -> Unit
 ) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+            .padding(padding)
+            .padding(Dimens.CommonPadding)
+            .verticalScroll(rememberScrollState())
+    ) {
+        ProfileSection(
+            myProfileUiState = uiState.myProfileSectionUiState,
+            onEditProfileClick = {  },
+        )
 
+        GrayDivider()
+    }
 }
--- feature/my/src/main/kotlin/project/priceit/feature/my/model/MyUiState.kt
+++ feature/my/src/main/kotlin/project/priceit/feature/my/model/MyUiState.kt
@@ -1,5 +1,11 @@
 package project.priceit.feature.my.model
 
-data class MyUiState (
+data class MyUiState(
+    val myProfileSectionUiState: MyProfileSectionUiState = MyProfileSectionUiState(),
+)
+
+data class MyProfileSectionUiState(
     val userName: String = "",
+    val profileImageUrl: String? = null,
+    val point: Int = 0
 )
