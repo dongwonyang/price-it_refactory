d5b45bb092d25b4dc7e42bacb1febb9f81cd99e8
dongwonyang <121853823+dongwonyang@users.noreply.github.com>
2026-02-12 00:41:57 +0900

    feat/#14: profileDialog ui 구현

--- feature/my/src/main/kotlin/project/priceit/feature/my/MyScreen.kt
+++ feature/my/src/main/kotlin/project/priceit/feature/my/MyScreen.kt
@@ -13,11 +13,14 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import project.priceit.core.designsystem.component.GrayDivider
 import project.priceit.core.designsystem.theme.Dimens
 import project.priceit.feature.my.component.ActivityHistorySection
+import project.priceit.feature.my.component.ProfileDialog
 import project.priceit.feature.my.component.ProfileSection
 import project.priceit.feature.my.model.MyEffect
 import project.priceit.feature.my.model.MyEvent
@@ -59,15 +62,43 @@ fun MyScreen(
             .padding(Dimens.CommonPadding)
             .verticalScroll(rememberScrollState())
     ) {
+        uiState.myProfileSectionUiState.run {
+            if (isDialog) {
+                ProfileDialog(
+                    currentNickname = userName,
+                    currentImageUrl = profileImageUrl,
+                    onDismiss = { onEvent(MyEvent.HideProfileDialog) },
+                    onConfirm = { newNickname, newImageUri ->
+                        onEvent(
+                            MyEvent.SetProfile(
+                                newNickname = newNickname,
+                                newImageUri = newImageUri
+                            )
+                        )
+                    }
+                )
+            }
+        }
+
         ProfileSection(
             myProfileUiState = uiState.myProfileSectionUiState,
-            onEditProfileClick = {  },
+            onEditProfileClick = { onEvent(MyEvent.ShowProfileDialog) },
         )
 
-        Spacer(modifier = Modifier.height(Dimens.DpMedium))g
+        Spacer(modifier = Modifier.height(Dimens.DpMedium))
         GrayDivider()
         Spacer(modifier = Modifier.height(Dimens.DpMedium))
 
         ActivityHistorySection()
     }
+}
+
+@Preview
+@Composable
+fun MyScreenPreview() {
+    MyScreen(
+        padding = PaddingValues(0.dp),
+        uiState = MyUiState(),
+        onEvent = {}
+    )
 }
