54a52b4fef99068a1da439a71fc36ad4f93c3718
dongwonyang <121853823+dongwonyang@users.noreply.github.com>
2026-02-25 16:32:46 +0900

    feat/#21: SearchUiState 변경
    searchUiSection 추가
    query, filterList 포함되어있음

---
+++ feature/search/src/main/kotlin/project/priceit/feature/search/model/FilterItem.kt
@@ -0,0 +1,6 @@
+package project.priceit.feature.search.model
+
+data class FilterItem(
+    val name: String,
+    val isSelected: Boolean = false
+)
--- feature/search/src/main/kotlin/project/priceit/feature/search/model/SearchUiState.kt
+++ feature/search/src/main/kotlin/project/priceit/feature/search/model/SearchUiState.kt
@@ -2,4 +2,14 @@ package project.priceit.feature.search.model
 
 data class SearchUiState (
     val userName: String = "",
+    val searchUiSection: SearchUiSection = SearchUiSection()
+)
+
+data class SearchUiSection(
+    val query: String = "",
+    val searchFilterList: List<FilterItem> = listOf(
+        FilterItem(name = "Region 1", isSelected = true),
+        FilterItem(name = "Region 2", isSelected = false),
+        FilterItem(name = "Region 3", isSelected = false),
+    )
 )
