11f8e2cf9e8defb2f4352996ed54177205eb5de7
dongwonyang <121853823+dongwonyang@users.noreply.github.com>
2026-03-03 19:15:37 +0900

    refac/#23: datasource core:network -> core:data로 변경
    datasource는 data 관리 계층이 맞다고 판단

--- core/data/src/main/kotlin/project/priceit/core/data/datasource/auth/RetrofitAuthDatasource.kt
+++ core/data/src/main/kotlin/project/priceit/core/data/datasource/auth/RetrofitAuthDatasource.kt
@@ -1,13 +1,11 @@
-package project.priceit.core.network.datasource.auth
+package project.priceit.core.data.datasource.auth
 
 import project.priceit.core.network.api.AuthApi
 import project.priceit.core.network.model.ApiResponse
 import project.priceit.core.network.model.response.LoginResponse
 import project.priceit.core.network.util.runRemote
 import javax.inject.Inject
-import javax.inject.Singleton
 
-@Singleton
 class RetrofitAuthDatasource @Inject constructor(
    private val authApi: AuthApi
 ): AuthDatasource {
